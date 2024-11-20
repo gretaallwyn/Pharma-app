9a10c906dcf110ad02b6b5752a91cc0373559f5d
gretaallwyn <68838318+gretaallwyn@users.noreply.github.com>
2024-11-20 18:23:11 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,1 @@
-import firebase from "firebase";
-var firebaseConfig = {
-    apiKey: "AIzaSyBvQVmLvvRBtMVR7jQ1oZt3XomIHJgCEFk",
-    authDomain: "self-designed-app-57aec.firebaseapp.com",
-    projectId: "self-designed-app-57aec",
-    storageBucket: "self-designed-app-57aec.appspot.com",
-    messagingSenderId: "407897452161",
-    appId: "1:407897452161:web:236c8c179d80ab3d916780"
-  };
- // Initialize Firebase 
- if(!firebase.apps.length){ firebase.initializeApp(firebaseConfig); }
- export default firebase.firestore();
+
